2c901bc83abaa0c7edc138e3bddbe05852b22706
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2014-03-03 12:39:02 -0700

    Further cleanup: move static assets into /public

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'bootstrap-sass'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "public/css"
 sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+images_dir = "public/img"
+javascripts_dir = "public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -17,7 +17,6 @@ javascripts_dir = "js"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
